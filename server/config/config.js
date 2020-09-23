646f9317466864a9369443392cda87ba8c3ef379
Ramon <rcanelav@gmail.com>
2020-09-23 22:10:36 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://rcanelav:OHLlWemsMUHvmJJc@cluster0.cjxlt.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB
